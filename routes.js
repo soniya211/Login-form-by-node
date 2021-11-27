8358d117f96fc0c0b74c357f0cd739b4e2421c23
soniya211 <65823100+soniya211@users.noreply.github.com>
2021-11-27 17:43:30 +0530

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const signUpTemplateCopy = require('../models/signupModels')
+
+router.post('/signup',(request, response) =>{
+   const signedUpUser = new signUpTemplateCopy({
+       fullName:request.body.fullName,
+       username:request.body.username,
+       email:request.body.email,
+       password:request.body.password,
+   })
+
+   signedUpUser.save()
+   .then(data =>{
+       response.json(data)
+   }) 
+   .catch(error =>{
+       response.json(error)
+   })
+})
+
+
+module.exports = router
+
